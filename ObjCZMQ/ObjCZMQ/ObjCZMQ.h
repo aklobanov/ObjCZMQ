39febbb25cedfebcf93cd7160aefb17f64b7fc3b
aklobanov <aklobanov@gmail.com>
2016-02-04 20:02:11 +0300

    Add code

---
+++ ObjCZMQ/ObjCZMQ/ObjCZMQ.h
@@ -0,0 +1,17 @@
+//
+//  ObjCZMQ.h
+//  ObjCZMQ
+//
+//  Created by ALEXEY LOBANOV on 03.02.16.
+//  Copyright © 2016 Blue Skies Software. All rights reserved.
+//
+
+//! Project version number for ObjCZMQ.
+FOUNDATION_EXPORT double ObjCZMQVersionNumber;
+
+//! Project version string for ObjCZMQ.
+FOUNDATION_EXPORT const unsigned char ObjCZMQVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ObjCZMQ/PublicHeader.h>
+
+
---
+++ ObjCZMQ/ObjCZMQ/ZMQError.m
@@ -0,0 +1,28 @@
+//
+//  ZMQError.m
+//  ObjCZMQ
+//
+//  Created by ALEXEY LOBANOV on 03.02.16.
+//  Copyright © 2016 Blue Skies Software. All rights reserved.
+//
+
+#import "ZMQError.h"
+#import "zmq.h"
+
+#define LOCAL_LEVEL_0 0
+#define LOCAL_LEVEL_1 1
+#define LOCAL_LEVEL_2 2
+
+
+@implementation ZMQError
+- (instancetype)init
+{
+#if DEBUG >= LOCAL_LEVEL_1
+    NSLog(@"Running %@ '%@'", self.class, NSStringFromSelector(_cmd));
+#endif
+    static NSString *domain = kZMQErrorDomain;
+    int code = zmq_errno();
+    self = [super initWithDomain:domain code:(NSInteger)code userInfo:@{NSLocalizedDescriptionKey:[NSString stringWithUTF8String:zmq_strerror(code)]}];
+    return self;
+}
+@end
